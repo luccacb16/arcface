5391fd91dfc6c67e20341ae2bc76fdb5eda8f0e6
luccacb16 <lucca.barberato@gmail.com>
2024-09-23 01:42:37 -0300

    remove clip grad norm

--- train.py
+++ train.py
@@ -68,7 +68,7 @@ def train(
             
             if (step + 1) % accumulation_steps == 0:
                 scaler.unscale_(optimizer)
-                epoch_norm += torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+                # += torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
                 
                 scaler.step(optimizer)
                 scaler.update()
@@ -81,7 +81,7 @@ def train(
         progress_bar.close()
         
         epoch_loss = running_loss / len(train_dataloader)
-        epoch_norm = epoch_norm / num_batches
+        #epoch_norm = epoch_norm / num_batches
         epoch_accuracy, epoch_precision, epoch_recall, epoch_f1, val_loss = evaluate(model, test_dataloader, criterion, dtype=dtype, device=device)
         
         if USING_WANDB:
